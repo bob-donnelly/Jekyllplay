0641d61ea17c890bd2baf9c478787b8e7ff6313e
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-09-16 12:48:24 +1200

    ROLL OUT

---
+++ index.js
@@ -0,0 +1,15 @@
+var truck = "Optimus Prime"
+
+console.log(truck)
+
+var lastname = "Autobots"
+
+console.log(lastname)
+
+var age = "2000000000000000000000000000000000"
+
+console.log(age)
+
+
+
+
