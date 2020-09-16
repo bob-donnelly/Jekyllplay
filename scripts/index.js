2c638755ab43e954e61e537381caca1ba36a98e2
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-09-17 10:38:34 +1200

    var input and create list

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("enter")
+var input = document.getElementById("input")
+var ul = document.querySelector("ul");
+
+function inputLength () {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+
+}
