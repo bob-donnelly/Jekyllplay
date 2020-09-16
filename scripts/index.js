b9265e213ca98e4b9d718f088fe27bcf3092edc9
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-09-17 11:25:36 +1200

    line 38

--- scripts/index.js
+++ scripts/index.js
@@ -11,13 +11,26 @@ function createListElement() {
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
 
-    li.addEventListener; "click", function () {
+    li.addEventListener("click", function (){
         var finished = this.classList.toggle("done");
         var removeButton = docoument.createElement("button");
-        var finished = this.classList.add("deletebutton");
-        if (finished) {
-            removeButton; appendChild(document createTextNode("remove"));
-        }
+        var finished = this.classList.add("deletebutton")}
+        )
     }
+        if (finished){
+            removeButton.appendChild(document.createTextNode("remove"));
+            removeButton.classList = "deletebutton";}
+            removeButton.addEventListener("click", function (){
+            li.appendChild(removeButton);
 
-}
+            removeButton.addEventListener("click", function () {
+                this.parentElement.remove();
+            });
+            
+            else {
+                this.getElementByClassName("deleteButton") [0].remove();
+            }
+            input.value ="";
+            
+            })
+          
