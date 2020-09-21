031674699140c120b103236a5a150388686665e8
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-09-22 11:57:45 +1200

    added my own comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,15 @@
+//Declaring the variables of the document objects
+
 var button = document.getElementById("enter");
 var input = document.getElementById("input");
 var ul = document.querySelector("ul");
 
+
 function inputLength() {
 	return input.value.length;
 }
+
+//Creates a list that pops in and out
 
 function createListElement() {
 	var li = document.createElement("li");
@@ -12,15 +17,11 @@ function createListElement() {
 	ul.appendChild(li);
 
 	li.addEventListener("click", function () {
-		// creates a boolean that toggles the done class on li:
-		// if the list item is clicked this toggles the done class
 		var finished = this.classList.toggle("done");
-		// creates a remove button for the finished item:
 		var removeButton = document.createElement("button");
 		removeButton.classList.add("deleteButton");
 
-		// if the list item is clicked (li add event listener ) then
-		// finished is true
+		
 		if (finished) {
 			removeButton.appendChild(document.createTextNode("remove"));
 			removeButton.classList = "deleteButton";
@@ -33,7 +34,6 @@ function createListElement() {
 			this.getElementsByClassName("deleteButton")[0].remove();
 		}
 	});
-	// revert input value back to nothing
 	input.value = "";
 }
 
