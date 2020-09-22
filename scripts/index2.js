442f80a1ac12eee4a6f6e2ad228e46ea09c8036a
bob-donnelly <bob-donnelly@live.nmit.ac.nz>
2020-09-22 12:26:41 +1200

    added code to my new script

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,4 +1,5 @@
 function createWindow() {
-    let url ="https://google.com";
+    let url ="https://firefox.com";
     let win = window.open(url, "My new window", "width=300, height=200")
+    document.getElementById("result").innerHTML= win.name + " - " + win.opener.location;
 }
